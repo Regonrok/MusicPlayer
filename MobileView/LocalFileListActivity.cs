29ec7f4687fa94e2421310674405dc57d94f269b
Mikhail Kutsan <Mishakutsan2011@mail.ru>
2018-06-14 09:41:17 +0700

    Start project

---
+++ MobileView/LocalFileListActivity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+
+namespace MobileView
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class LocalFileListActivity : Activity
+    {
+        private List<string> _countries = new List<string>() { "country 1", "country 1" , "country 1" , "country 1" , "country 1" };
+
+        RecyclerView mRecyclerView;
+        RecyclerView.LayoutManager mLyoutManager;
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.list_local_files);
+
+
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.NavigationItemSelected += (s, e) =>
+            {
+                switch (e.Item.ItemId)
+                {
+                    case Resource.Id.music_player_dashboard:
+                        //start LocalFilesActivity
+                        Intent player = new Intent(this, typeof(MainActivity));
+                        StartActivity(player);
+                        break;
+                    case Resource.Id.local_files_dashboard:
+                        //
+                        Toast.MakeText(this, Resource.String.local_dashboard,ToastLength.Short).Show();
+                        break;
+                }
+            };
+
+        }
+
+    }
+}
---
+++ MobileView/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using Android.Support.V7.App;
+using Android.Support.Design.Widget;
+using Android.Content;
+
+namespace MobileView
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private TextView _textMessage;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            _textMessage = FindViewById<TextView>(Resource.Id.message);
+
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.NavigationItemSelected += (s, e) =>
+            {
+                switch (e.Item.ItemId)
+                {
+                    case Resource.Id.music_player_dashboard:
+                        // Create message for user
+                        Toast.MakeText(this, Resource.String.player_dashboard, ToastLength.Short).Show();
+                        break;
+                    case Resource.Id.local_files_dashboard:
+                        //start LocalFilesActivity
+                        Intent localFiles = new Intent(this, typeof(LocalFileListActivity));
+                        StartActivity(localFiles);
+                        break;
+                }
+            };
+        }
+    }
+}
+
